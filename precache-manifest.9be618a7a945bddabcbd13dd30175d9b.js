332777564f041e0db40c5025fa151cd873df391d
Faizan ul haq <79333547+ulhaqFaizan@users.noreply.github.com>
2023-01-09 16:43:13 +0500

    Add files via upload

---
+++ precache-manifest.9be618a7a945bddabcbd13dd30175d9b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81ef4c7c852c20de88e07d74abafc63",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "032c0892d6383f4037f5",
+    "url": "/keeper-app/static/js/2.8a3589a0.chunk.js"
+  },
+  {
+    "revision": "fd93c18019e219823d6f",
+    "url": "/keeper-app/static/js/main.54287dfc.chunk.js"
+  },
+  {
+    "revision": "7497e34bc9a978950d71",
+    "url": "/keeper-app/static/js/runtime-main.5f33c95a.js"
+  }
+]);
